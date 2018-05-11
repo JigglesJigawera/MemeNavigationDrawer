9f209db4912366b4236a73457fa4dd9b2651e3cc
Joshua Giguere <18gigjos@student.mcpsmt.org>
2018-05-11 10:18:05 -0600

    somethings

--- app/src/main/java/examples/pltw/org/collegeapp/ApplicantActivity.java
+++ app/src/main/java/examples/pltw/org/collegeapp/ApplicantActivity.java
@@ -80,12 +80,6 @@ public class ApplicantActivity extends AppCompatActivity
 
 
 
-
-
-
-
-
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
